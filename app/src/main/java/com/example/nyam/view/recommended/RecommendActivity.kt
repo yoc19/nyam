c4a2fb4bbaa5bbe06bbde8a0a96b45ba89f10004
yoh <yohaneschristianto19@gmail.com>
2024-11-26 19:13:52 +0700

    Add Activity

---
+++ app/src/main/java/com/example/nyam/view/recommended/RecommendActivity.kt
@@ -0,0 +1,21 @@
+package com.example.nyam.view.recommended
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.nyam.R
+
+class RecommendActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_recommend)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
